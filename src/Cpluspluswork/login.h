b91b2bd77c726889a71b5825b6e4c699b3ee3454
unknown <zhouluhappy97@126.com>
2016-09-02 14:56:02 +0800

    Add mainwindow(login) files

---
+++ src/Cpluspluswork/login.h
@@ -0,0 +1,24 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+#include<QDialog>
+
+namespace Ui
+{
+    class login;
+}
+
+class login:public:QDialog
+{
+    Q_OBJECT
+public:
+    login(QWidget *parent =0);
+    ~login();
+    int identify;
+private:
+    Ui::login *uiptr;
+private slots:
+    void on_logButton_clicked();
+    void on_newButton_clicked();
+};
+
+#endif // LOGIN_H
---
+++ src/Cpluspluswork/mainwindow.cpp
@@ -0,0 +1,81 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include"login.h"
+#include<QtGui>
+#include<QtSql/QSqlQuery>
+#include<QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->lineEdit2->setEchoMode(QLineEdit::Password);//密码设置为不可见
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_logButton_clicked()//用户点击登录按钮
+{
+    if(ui->comboBox->currentText()=="Passenger")//身份为乘客
+    {
+        QSqlQue query1;
+        query1.prepare("select *from passengers where paName=?");
+        query1.addBindValue(ui->lineEdit1->text());
+        query1.exec();
+        if (query1.next())//在数据库中查到了该用户名,开始检测密码是否正确
+        {
+            int password=query1.value(1).toInt;
+            if(password!=ui->lineEdit2->text().toInt())//密码错误，发出警告信息
+            {
+                QMessage::warning(this,tr("登录失败！"),tr("密码错误"),QMessageBox::Close);
+            }
+            else//用户名和密码都正确
+            {
+                identify=0;//身份为乘客
+                accept();
+            }
+        }
+        else//用户名不存在，发出警告
+        {
+            QMessage::warning(this,tr("登录失败！"),tr("用户名不存在"),QMessageBox::Close);
+        }
+    }
+    else//身份为管理员
+    {
+        if(ui->lineEdit1->text()=="Admin"&&ui->lineEdit2->text()=="123456")
+        {
+            identify=1;//标记身份为管理员
+            accept();
+        }
+        else
+            QMessage::warning(this,tr("登录失败！"),tr("用户名或密码错误"),QMessageBox::Close);
+    }
+    ui->lineEdit1->clear();//清空用户名输入框
+    ui->lineEdit2->clear();//清空密码输入框
+    ui->lineEdit1->setFocus();//将光标转到用户名输入框
+}
+
+void MainWindow::on_newButton_clicked()//用户点击注册按钮
+{
+    QSqlQuery query;
+    QSqlQuery query1;
+    query1.prepare("select * from passengers where paName=?");
+    query1.addBindValue(ui->lineEdit1->text());
+    query1.exec();
+    if(query1.next())//用户名已被注册，发出警告信息
+    {
+        QMessageBox::warning(this,tr("注册失败！"),tr("该用户名已被注册"),QMessageBox::Close);
+    }
+    else
+    {
+        query.prepare("insert into passengers values(?,?)");
+        query.addBindValue(ui->lineEdit1->text());
+        query.addBindValue(ui->lineEdit2->text().toInt());
+        query.exec();
+    }
+    ui->lineEdit1->clear();
+    ui->lineEdit2->clear();
+}
